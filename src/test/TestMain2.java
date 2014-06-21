ef204db675437b397317b59ccd7ae45f7ada0b6d
xiangwu <xiang.wu@dianping.com>
2014-06-21 23:57:35 +0800

    add TestMain2

---
+++ src/test/TestMain2.java
@@ -0,0 +1,26 @@
+package test;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class TestMain2 {
+	public AtomicInteger counter = new AtomicInteger(0);
+
+	public static void main(String[] args) throws Exception {
+		TestMain main = new TestMain();
+		for (int i = 0; i < 10; i++) {
+			new Thread(main.new Client()).start();
+		}
+		System.in.read();
+	}
+
+	class Client implements Runnable {
+
+		@Override
+		public void run() {
+			while (true) {
+				System.out.println(counter.addAndGet(1));
+			}
+		}
+
+	}
+}
